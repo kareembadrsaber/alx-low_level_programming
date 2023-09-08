3a668068e8d5fb182902465e55e313a39b0e773f
kareem badr saber <kimobadrsaber@gmail.com>
2023-09-08 03:49:30 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,20 +2,23 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char c;
-    c = 'a';
+	char c;
 
-    while (c <= 'z') {
-        putchar(c);
-        c++;
-    }
+	c = 'a';
 
-    putchar('\n');
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
 
-    return (0);
+	putchar('\n');
+
+	return (0);
 }
 
