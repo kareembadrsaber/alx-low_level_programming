1d2cc6d687d2405a04cec661a67dfe66bd25a194
kareem badr saber <kimobadrsaber@gmail.com>
2023-09-12 02:22:17 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
-#include "putchar.h"
+#include "main.h"
+
+/**
+* main - Print the word "main"
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
+    int i;
+    char p[] = "_putchar";
+
+    for (i = 0; i < 8; i++)
+    {
+        _putchar(p[i]);
+    }
     _putchar('\n');
     return (0);
 }
-
