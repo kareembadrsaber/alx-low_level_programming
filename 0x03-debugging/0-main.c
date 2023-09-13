c392da0a7ff06cdb7aca1a9142a7771dbc9b9b9b
kareem badr saber <kimobadrsaber@gmail.com>
2023-09-12 23:12:49 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * main - Tests the function that prints if an integer is positive or negative
- *
+ * main - Tests the positive_or_negative function
  * Return: Always 0
  */
-
 int main(void)
 {
     int i;
-
-    i = 0;  /* Change this line to test with 0 */
+    
+    i = 0;
     positive_or_negative(i);
-
+    
     return (0);
 }
