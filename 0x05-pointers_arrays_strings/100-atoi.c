733a1de9937ceb5d0229e9ac1d5feeedc6869c72
kareem badr saber <kimobadrsaber@gmail.com>
2023-09-18 00:45:37 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,35 +8,42 @@
  */
 int _atoi(char *s)
 {
-	int i, digit, len, sign, found_digit;
+	int result = 0;
+	int sign = 1; /* 1 represents positive sign by default */
+	int i = 0;
 
-	i = 0;
-	digit = 0;
-	len = 0;
-	sign = 1;
-	found_digit = 0;
+	/* Handle leading spaces */
+	while (s[i] == ' ')
+		i++;
 
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len)
+	/* Handle the sign (positive/negative) */
+	if (s[i] == '-')
 	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			digit *= sign;
-			digit += found_digit * 10;
-			found_digit = 1;
-		}
-		else if (found_digit)
-		{
-			break;
-		}
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
 		i++;
 	}
 
-	return (digit);
+	/* Process the digits and build the result */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		int digit = s[i] - '0';
+
+		/* Check for potential overflow */
+		if (result > (INT_MAX - digit) / 10)
+		{
+			if (sign == 1)
+				return INT_MAX;
+			else
+				return INT_MIN;
+		}
+
+		result = result * 10 + digit;
+		i++;
+	}
+
+	return result * sign;
 }
