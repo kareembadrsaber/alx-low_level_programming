f0d6cdadc28368f1985975750691254728b16ab9
kareem badr saber <kimobadrsaber@gmail.com>
2023-09-20 03:00:07 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/102-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  /*
+   * Replace the next line with one line of code so that it prints 98\n
+   * by indirectly modifying a[2] through the pointer p.
+   * You are not allowed to use a, modify p, or add any other code.
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
+}
